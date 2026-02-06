0e4dab80bac07556b4feb625ddfcf970d2926a05
Sibtain Raza <khanbhai14063@gmail.com>
2026-02-06 22:22:08 +0530

    i Added some function and css also to make good

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,25 +1,26 @@
 import React from 'react'
 import { Bookmark } from 'lucide-react';
 
-const Card = () => {
+
+const Card = (props) => {
     return (
         <div className="card">
             <div className="top">
-                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQV5wT5R7vumVlrrcVIzxKqoqNYtNmpJqwMIQ&s" alt="Amazon-Icon" />
+                <img src={props.brandLogo} alt="Amazon-Icon" />
                 <button>Save <Bookmark /></button>
             </div>
             <div className="center">
-                <h3>Amazon <span>% dayd ago</span></h3>
-                <h2>Senior UI/UX Designer</h2>
+                <h3>{props.company} <span>{props.datePosted}</span></h3>
+                <h2>{props.post}</h2>
                 <div className='tag'>
-                    <h4>Part Time</h4>
-                    <h4>Senior Level</h4>
+                    <h4>{props.tag1}</h4>
+                    <h4>{props.tag2}</h4>
                 </div>
             </div>
             <div className="bottom">
                 <div>
-                    <h3>$120/hr</h3>
-                    <p>Mumbai, India</p>
+                    <h3>${props.pay}</h3>
+                    <p>{props.location}</p>
                 </div>
                 <button>Apply Now</button>
             </div>
